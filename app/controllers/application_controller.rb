93db65d076a9db012cf0497b0047e80bb8773df4
Lovinder Pnag <lspnag@gmail.com>
2015-05-07 11:09:16 -0700

    added helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,10 +7,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def current_user
-  	
+  	@user ||= User.find_by(id: session[:user_id]) if session[:user_id]
   end
 
   def logged_in?
-  	
+  	!(session[:user_id].nil?)
   end
 end
